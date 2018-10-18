2daa5b4619880237470a3b9d358f3fc008e3899a
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-10-18 11:53:17 -0500

    configured draft requests and sagas for post and delete

--- src/redux/sagas/itemSaga.js
+++ src/redux/sagas/itemSaga.js
@@ -1,6 +1,6 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import { ITEM_ACTIONS } from '../actions/itemActions';
-import { callItemGet } from '../requests/itemRequests';
+import { callItemGet, callItemDelete, callItemPost } from '../requests/itemRequests';
 
 function* fetchItems() {
   try {
@@ -13,9 +13,29 @@ function* fetchItems() {
   }
 }
 
+function* deleteItem(action) {
+  try {
+    yield callItemDelete(action.payload);
+    yield put({ type: ITEM_ACTIONS.GET_ITEMS });
+  } catch (error) {
+    console.log('Error deleting items', error);
+  }
+}
+
+function* addItem(action) {
+  try {
+    yield callItemPost(action.payload);
+    yield put({ type: ITEM_ACTIONS.GET_ITEMS });
+  } catch (error) {
+    console.log('Error adding item', error);
+  }
+}
+
 
 function* itemSaga() {
   yield takeLatest(ITEM_ACTIONS.GET_ITEMS, fetchItems);
+  yield takeLatest(ITEM_ACTIONS.DELETE_ITEM, deleteItem);
+  yield takeLatest(ITEM_ACTIONS.ADD_ITEM, addItem);
 }
 
 export default itemSaga;
