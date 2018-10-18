aab81d1cb2f45fba36c188142aa47a822ca087c7
Christopher Brenberg <cbrenberg@gmail.com>
2018-10-18 14:58:26 -0500

    style count table

--- src/components/CountTable/CountTable.js
+++ src/components/CountTable/CountTable.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
+import { Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { ITEM_ACTIONS } from '../../redux/actions/itemActions';
 
@@ -10,25 +10,35 @@ class CountTable extends Component {
 
 
   render() {
+
+    const styles = {
+      paper: {
+        minWidth: '40vw',
+        maxWidth: '250px',
+      },
+    };
+
     return (
-      <Table >
-        <TableHead>
-          <TableRow>
-            <TableCell>Username</TableCell>
-            <TableCell>Number of Shelf Items</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {this.props.state.itemList.count.map(item => {
-            return (
-              <TableRow key={item.id}>
-                <TableCell>{item.username}</TableCell>
-                <TableCell>{item.number_of_items}</TableCell>
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
+      <Paper style = {styles.paper}>
+        <Table >
+          <TableHead>
+            <TableRow>
+              <TableCell>Username</TableCell>
+              <TableCell>Number of Shelf Items</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.props.state.itemList.count.map(item => {
+              return (
+                <TableRow key={item.id}>
+                  <TableCell>{item.username}</TableCell>
+                  <TableCell>{item.number_of_items}</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
     )
   }
 }
